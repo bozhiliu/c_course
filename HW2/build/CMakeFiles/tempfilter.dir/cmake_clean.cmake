427ffddacd2cd1327fcb26fd00405b26da3ea33c
Bozhi <bozhiliu@email.arizona.edu>
2015-09-16 13:51:49 -0700

    change file directory structure & debug

---
+++ HW2/build/CMakeFiles/tempfilter.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tempfilter.dir/card.o"
+  "CMakeFiles/tempfilter.dir/dlist.o"
+  "CMakeFiles/tempfilter.dir/main.o"
+  "CMakeFiles/tempfilter.dir/output.o"
+  "CMakeFiles/tempfilter.dir/replace.o"
+  "CMakeFiles/tempfilter.dir/str.o"
+  "CMakeFiles/tempfilter.dir/test.o"
+  "tempfilter.pdb"
+  "tempfilter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tempfilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW2/src/output.h
@@ -0,0 +1,18 @@
+/*
+ * output.h
+ *
+ *  Created on: Sep 16, 2015
+ *      Author: vito
+ */
+
+#ifndef OUTPUT_H_
+#define OUTPUT_H_
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "dlist.h"
+
+void write_output(FILE * output, DList * list);
+
+
+#endif /* OUTPUT_H_ */
