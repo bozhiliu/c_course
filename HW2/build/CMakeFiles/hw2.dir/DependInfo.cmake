33dbf5c27efd84b46abc352adbf489633349ae53
bozhiliu <bozhiliu@compute.engr.arizona.edu>
2015-09-19 14:01:40 -0700

    restructure

--- HW2/build/CMakeFiles/hw2.dir/DependInfo.cmake
+++ HW2/build/CMakeFiles/hw2.dir/DependInfo.cmake
@@ -1,25 +1,22 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "C"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_C
-  "/home/vito/workspace1/HW2/card.c" "/home/vito/workspace1/HW2/build/CMakeFiles/hw2.dir/card.o"
-  "/home/vito/workspace1/HW2/dlist.c" "/home/vito/workspace1/HW2/build/CMakeFiles/hw2.dir/dlist.o"
-  "/home/vito/workspace1/HW2/main.c" "/home/vito/workspace1/HW2/build/CMakeFiles/hw2.dir/main.o"
-  "/home/vito/workspace1/HW2/output.c" "/home/vito/workspace1/HW2/build/CMakeFiles/hw2.dir/output.o"
-  "/home/vito/workspace1/HW2/replace.c" "/home/vito/workspace1/HW2/build/CMakeFiles/hw2.dir/replace.o"
-  "/home/vito/workspace1/HW2/str.c" "/home/vito/workspace1/HW2/build/CMakeFiles/hw2.dir/str.o"
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bozhiliu/m0/HW2/src/card.c" "/home/bozhiliu/m0/HW2/build/CMakeFiles/hw2.dir/card.o"
+  "/home/bozhiliu/m0/HW2/src/dlist.c" "/home/bozhiliu/m0/HW2/build/CMakeFiles/hw2.dir/dlist.o"
+  "/home/bozhiliu/m0/HW2/src/main.c" "/home/bozhiliu/m0/HW2/build/CMakeFiles/hw2.dir/main.o"
+  "/home/bozhiliu/m0/HW2/src/output.c" "/home/bozhiliu/m0/HW2/build/CMakeFiles/hw2.dir/output.o"
+  "/home/bozhiliu/m0/HW2/src/replace.c" "/home/bozhiliu/m0/HW2/build/CMakeFiles/hw2.dir/replace.o"
+  "/home/bozhiliu/m0/HW2/src/str.c" "/home/bozhiliu/m0/HW2/build/CMakeFiles/hw2.dir/str.o"
   )
-SET(CMAKE_C_COMPILER_ID "GNU")
-
-# Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
-  )
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_C_TARGET_INCLUDE_PATH
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
--- HW2/build/CMakeFiles/hw2.dir/cmake_clean.cmake
+++ HW2/build/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -1,15 +1,15 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/hw2.dir/output.o"
-  "CMakeFiles/hw2.dir/str.o"
-  "CMakeFiles/hw2.dir/main.o"
-  "CMakeFiles/hw2.dir/replace.o"
+file(REMOVE_RECURSE
   "CMakeFiles/hw2.dir/card.o"
   "CMakeFiles/hw2.dir/dlist.o"
+  "CMakeFiles/hw2.dir/main.o"
+  "CMakeFiles/hw2.dir/output.o"
+  "CMakeFiles/hw2.dir/replace.o"
+  "CMakeFiles/hw2.dir/str.o"
   "hw2.pdb"
   "hw2"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang C)
-  INCLUDE(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang C)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
